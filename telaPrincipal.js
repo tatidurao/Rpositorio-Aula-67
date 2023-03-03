0c9573c31592fabce312f58a5475fd473e96c4d2
tatidurao <tatila.bd@gmail.com>
2023-03-03 14:34:59 -0300

    tela principa2l

--- telaPrincipal.js
+++ telaPrincipal.js
@@ -4,3 +4,9 @@ var endereço = "Rua das Ruas"
 var cidade = "São Paulo"
 var pais = "Brasil"
 
+
+var nome = "Tati"
+var idade = 29
+var endereço = "Rua das Ruas"
+var cidade = "São Paulo"
+var pais = "Brasil"
